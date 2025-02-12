6b9cfefefc9f5839bdedf903b7fed97436cccb82
prathamaaaaaa <prathmasangani@gmail.com>
2025-02-12 11:19:45 +0530

    port change

--- src/main/java/com/example/demo/StudentSystemManagementApplication.java
+++ src/main/java/com/example/demo/StudentSystemManagementApplication.java
@@ -1,7 +1,10 @@
 package com.example.demo;
 
+import org.hibernate.cfg.Environment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 public class StudentSystemManagementApplication {
@@ -9,5 +12,12 @@ public class StudentSystemManagementApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(StudentSystemManagementApplication.class, args);
 	}
+	@PostConstruct
+	public void logPort() {
+	    System.out.println("Application running on port: " + System.getProperty("server.port"));
+        System.out.println("Server Port: " + Environment.getProperties());
+
+	}
+
 
 }
